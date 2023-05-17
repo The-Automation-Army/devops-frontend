e9aea6c72347f1eae88d6c1a10fc86ac1de54789
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-05-16 23:45:45 -0600

    :sparkles: add zoo services

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { LoginRequest, LoginResponse, User } from '../models/user';
+import { environment } from 'src/environments/environment';
+import { StorageService } from './storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  constructor(
+    private httpClient: HttpClient,
+    private storageService: StorageService
+  ) {}
+
+  register(user: User): Observable<User> {
+    return this.httpClient.post<User>(`${environment().apiURL}/register`, user);
+  }
+
+  login(user: LoginRequest): Observable<LoginResponse> {
+    return this.httpClient
+      .post<LoginResponse>(`${environment().apiURL}/login`, user)
+      .pipe(
+        tap((res: LoginResponse) => this.storageService.saveToken(res.token))
+      );
+  }
+
+  logout() {
+    this.storageService.removeToken();
+  }
+}
