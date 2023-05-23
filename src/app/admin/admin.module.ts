48c7d42edebcb1b85f63a4a638f5490ed3a59bfc
Jahir Gómez <jahirgomez1714@gmail.com>
2023-05-23 10:45:53 -0600

    ➕ Adding modules.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,6 +8,9 @@ import { EditAnimalComponent } from './pages/edit-animal/edit-animal.component';
 import { AnimalFormComponent } from './components/animal-form/animal-form.component';
 import { CreateAnimalComponent } from './pages/create-animal/create-animal.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SupplierDetailComponent } from './components/supplier-detail/supplier-detail.component';
+import { SupplierFormComponent } from './components/supplier-form/supplier-form.component';
+import { CreateSupplierComponent } from './pages/create-supplier/create-supplier.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,10 @@ import { HttpClientModule } from '@angular/common/http';
     EditAnimalComponent,
     AnimalFormComponent,
     CreateAnimalComponent,
+    SupplierDetailComponent,
+    EditAnimalComponent,
+    SupplierFormComponent,
+    CreateSupplierComponent,
   ],
   imports: [
     CommonModule,
