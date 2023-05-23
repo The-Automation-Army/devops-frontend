babd604723841fbeb6b3ae76bed9fd380506ade1
Jahir Gómez <jahirgomez1714@gmail.com>
2023-05-22 23:45:27 -0600

    ➕ Adding habitat model.

---
+++ src/app/admin/models/habitat.ts
@@ -0,0 +1,32 @@
+import { Animal } from "./animal";
+
+export interface Habitat {
+    id?: string;
+    size: number;
+    capacity: number;
+    area: string;
+    foodPercentage: number;
+    //zookeepers: Zookeeper[];
+    animals: Animal[];
+}
+
+export type HabitatRequest = Omit<Habitat, 'id'>;
+
+export const initialHabitatState: Habitat = {
+    id: '',
+    size: 0,
+    capacity: 0,
+    area: '',
+    foodPercentage: 0,
+    //zookeepers: [],
+    animals: [],
+};
+
+export const initialHabitatRequest: HabitatRequest = {
+    size: 0,
+    capacity: 0,
+    area: '',
+    foodPercentage: 0,
+    //zookeepers: [],
+    animals: [],
+};
