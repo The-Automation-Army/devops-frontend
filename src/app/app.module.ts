3fd9bcf958936d16d1f072f49591190460faad5d
Jahir Gómez <jahirgomez1714@gmail.com>
2023-05-29 17:25:15 -0600

    ⚙ Adding global zookeepr module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { environment } from 'src/environments/environment';
 import { AuthService } from './services/auth.service';
 import { SuppliersPageComponent } from './admin/pages/suppliers-page/suppliers-page.component';
 import { HabitatsPageComponent } from './admin/pages/habitats-page/habitats-page.component';
+import { ZookeepersPageComponent } from './admin/pages/zookeepers-page/zookeeper-page.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { HabitatsPageComponent } from './admin/pages/habitats-page/habitats-page
     LoginPageComponent,
     RegisterFormComponent,
     RegisterPageComponent,
+    ZookeepersPageComponent
   ],
   imports: [
     BrowserModule,
